e8a95ef8ca4248470597f1554369f1172a7b7ff2
Paul <paulanunaso@gmail.com>
2022-06-04 17:50:43 -0700

    trying

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *  *main - entry point
+ *   *Description: random number in n +ve/-ve
+ *    *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int firstSLetter, firstCletter, n;
+
+	for (firstSLetter = 97, n = 1; n < 27; firstSLetter++, n++)
+
+	{
+
+		putchar(firstSLetter);
+
+	}
+
+	for (firstCletter = 65, n = 1; n < 27; firstCletter++, n++)
+
+	{
+
+		putchar(firstCletter);
+
+	}
+
+	putchar(10);
+
+	return (0);
+}
